0fbdf55ebfd076599dbbdc7fbfc84da52453b694
sunzn <sunzn.cn@gmail.com>
2020-05-08 18:06:04 +0800

    first commit

--- Library/src/main/java/com/sunzn/window/library/IFloatWindowImpl.java
+++ Library/src/main/java/com/sunzn/window/library/IFloatWindowImpl.java
@@ -97,7 +97,6 @@ public class IFloatWindowImpl extends IFloatWindow {
 
     @Override
     public void hide() {
-        FloatLifecycle.show = false;
         if (once || !isShow) {
             return;
         }
